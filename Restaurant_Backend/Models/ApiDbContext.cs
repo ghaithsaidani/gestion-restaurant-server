bdfb931f32b19dbfe51d7fc4eaa44b92df4f0481
khemiri22 <khe.amine22@gmail.com>
2023-12-07 20:10:36 +0100

    Ajoutez des fichiers projet.

---
+++ Restaurant_Backend/Models/ApiDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Server_Side.Models
+{
+    public class ApiDbContext:DbContext
+    {
+        public ApiDbContext(DbContextOptions option) : base(option) { }
+    }
+}
